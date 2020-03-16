aec26e5fc3dfeb97e18853dad027cf8a2381b8c5
Amjimenezv94 <amjimenezv@gmail.com>
2020-03-16 13:48:15 -0500

    Initial commit 4

---
+++ 1-programacion-uno/reto3/retoTresProgramacionUno.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var parrafoElement = document.querySelector('.parrafo'); // lee mi etiqueta parrafo 
+parrafoElement.innerHTML = parrafoElement.innerHTML + ' Mundo'; // Adiciona la palabra mundo
---
+++ 1-programacion-uno/reto4/retoCuatroProgramacionUno.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var h1Element = document.querySelector('h1'); // lee mi etiqueta parrafo 
+
+h1Element.innerHTML = h1Element.innerHTML + 'Lola';
+// Adiciona la palabra mundo
